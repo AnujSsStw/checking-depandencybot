286c629156c059929f08296ac8f656bf9bdb592e
Anuj Singh <92370865+AnujSsStw@users.noreply.github.com>
2022-04-07 10:43:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// Object.assign{
+//     "ts-node": "10.4.0",
+//     "@types/node": "^16.11.10",     
+//     typescript: "4.5.2",
+// }
+
+
+// create an object with the following properties
+// name: string
+// age: number
+const person = {
+    "ts-node": "10.4.0",
+    "@types/node": "^16.11.10",     
+    typescript: "4.5.2",
+}
